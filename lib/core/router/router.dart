ab120fb29ca14f1dc6b0c13cee5f467e3494da98
EuclidTevvyalll <danilminnihanov@gmail.com>
2026-05-10 19:10:51 +0300

    Исправление ошибки роутинга: маршрут userProfile вынесен на верхний уровень

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -96,14 +96,6 @@ final routerProvider = Provider<GoRouter>((ref) {
             path: Routes.profile,
             builder: (context, state) => const ProfileScreen(),
           ),
-          GoRoute(
-            path: Routes.userProfile,
-            parentNavigatorKey: _rootNavigatorKey,
-            builder: (context, state) {
-              final userId = state.pathParameters['userId']!;
-              return ProfileScreen(userId: userId);
-            },
-          ),
         ],
       ),
       GoRoute(
@@ -127,6 +119,14 @@ final routerProvider = Provider<GoRouter>((ref) {
         builder: (context, state) {
           final id = state.pathParameters['roomId']!;
           return ChatInfoScreen(roomId: id);
+        },
+      ),
+      GoRoute(
+        path: Routes.userProfile,
+        parentNavigatorKey: _rootNavigatorKey,
+        builder: (context, state) {
+          final userId = state.pathParameters['userId']!;
+          return ProfileScreen(userId: userId);
         },
       ),
 
